64ea536cd55cd6a77ce63ae02275bdc520f2ed5d
Toewaioo <sawtoewaioo2003@gmail.com>
2025-08-06 19:35:29 +0630

    update app

--- src/component/Download.jsx
+++ src/component/Download.jsx
@@ -1,6 +1,7 @@
-const APKPURE_URL = "https://www.mediafire.com/file/gn6f8j026yd17sc/LuGyi+1.2.apk/file";
+const APKPURE_URL =
+  "https://www.mediafire.com/file_premium/wruj32txern7t00/LuGyi_1.3.apk/file";
 const DIRECT_APK_URL =
-  "https://github.com/Liar2003/lugyi-site/releases/download/v1.2.0/app-release.apk";
+  "https://github.com/Liar2003/lugyi-site/releases/download/v1.3.0/app-release.apk";
 const TELEGRAM_URL = "https://t.me/LuGyiandYoteshinMovies/2";
 
 // Images from placeholder context
